29cbcb8d407a7d2ba9fa952e548fe3e10e435b50
SamLangTen <samlangten@outlook.com>
2021-01-24 19:41:17 +0800

    add pack

---
+++ src/pack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Welcome to PACK by SamLangTen!"
+echo "-----------------------------"
+echo "PACK can pack subfolders into ZIP."
+echo "NOTICE: Make sure that command \"zip\" has been installed!"
+echo ""
+
+if [ -n "$1" ]; then
+    PACK_PATH=$1
+else
+    PACK_PATH=$(pwd)
+fi
+cd ${PACK_PATH}
+
+dir_count=$(ls -l | grep "^d" | wc -l)
+i=1
+echo "${DIR_COUNT} folder(s) need to be packed in total."
+echo ""
+ls ${PACK_PATH} | while read $LINE
+do
+    if [ -f "${PACK_PATH}/$LINE" ]; then
+        continue;
+    fi
+    echo -n "($i/${DIR_COUNT})Packing ${PACK_PATH}/$LINE into ${PACK_PATH}/$LINE.zip...";
+    cd "$LINE";
+    zip "$LINE.zip" * > /dev/null;
+    mv "$LINE.zip" ../
+    echo "Done!"
+    cd ..;
+    i=$(( $i + 1 ))
+done
