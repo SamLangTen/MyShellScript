0cf03ecae8a4eef14c739d0db7a295b12f8c6284
samlangten <samlangten@outlook.com>
2024-02-02 22:44:51 +0800

    update cf srv dns

---
+++ src/cloudflare-srv-dns/cloudflare-srv-dns.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# A bash script to update a Cloudflare DNS SRV record with a specific port
+# Used to provide DDNS service for my Minecraft server through IPv4 STUN
+# Needs the DNS record pre-creating on Cloudflare
+
+# Proxy - uncomment and provide details if using a proxy
+#export https_proxy=http://<proxyuser>:<proxypassword>@<proxyip>:<proxyport>
+
+# Cloudflare zone is the zone which holds the record
+zone=${HOOK_ZONE}
+# dnsrecord is the A record which will be updated
+dnsrecord=${HOOK_RECORD}
+port=${HOOK_PORT}
+## Cloudflare authentication details
+## keep these private
+cloudflare_auth_email=${HOOK_CF_EMAIL}
+cloudflare_auth_key=${HOOK_CF_KEY}
+
+# if here, the dns record needs updating
+
+# get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "Zoneid for $zone is $zoneid"
+
+# get the dns record id
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=SRV&name=$dnsrecord" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+# update the record
+curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"SRV\",\"data\":{\"port\":$port}}" | jq
+     
